ab27bf3a4a7ab3e87b0852bce8a867bb1cef758c
Naziullah Shawan <76747042+naziullah135@users.noreply.github.com>
2023-06-26 10:52:39 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-    <h1 className="text-3xl font-bold underline">Hello</h1>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
